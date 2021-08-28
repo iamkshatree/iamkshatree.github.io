74a4dc3730d0ca786420b490df2ec00026d52d4f
theArjun <be2017se573@gces.edu.np>
2021-08-28 22:45:23 +0545

    Initial commit

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,15 @@
+// #f5dadf
+let copyright = document.querySelector('.md-footer-copyright');
+copyright.innerHTML="";
+
+let header = document.querySelector('.md-header')
+header.style.backgroundColor = '#f5dadf';
+header.style.color='#000';
+
+let header_md = document.querySelector('.md-nav__title')
+header_md.style.backgroundColor = '#f5dadf';
+header_md.style.color='#000';
+
+let tabs = document.querySelector('.md-tabs')
+tabs.style.backgroundColor = '#f5dadf';
+tabs.style.color='#000';
